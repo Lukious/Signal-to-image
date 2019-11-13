7083479aca6fbd9b6a76cdcd727968d415e056fa
Lukious <zhsjzhsj@gmail.com>
2019-11-14 04:27:48 +0900

    Slicer + main is updated

--- slicer.py
+++ slicer.py
@@ -6,11 +6,11 @@ Created on Thu Jul 18 17:16:43 2019
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
-from matplotlib import pyplot as plt
 from scipy import signal
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 save_number = 0
 
@@ -25,13 +25,13 @@ peak_use_data = pd.DataFrame(ecg_peak_data,columns=["Sample"])
 peak_type_data = pd.DataFrame(ecg_peak_data,columns=["Type"])
 
 
-test_for_save = ecg_use_data.ix[peak_use_data.iloc[0][0]:peak_use_data.iloc[1][0]]
+#test_for_save = ecg_use_data.ix[peak_use_data.iloc[0][0]:peak_use_data.iloc[1][0]]
 
-
+#cut by peak and interpolation to 784 (for 28*28 img)
 for i in range(peak_use_data.shape[0]-1):
     #print(peak_use_data.iloc[i][0])
     for_save = ecg_use_data.ix[peak_use_data.iloc[i][0]:peak_use_data.iloc[i+1][0]]
-    for_save = signal.resample(for_save, 900)
+    for_save = signal.resample(for_save, 784)
     for_save=pd.DataFrame(for_save)
     if peak_type_data.iloc[i][0] == "+":
         for_save.to_csv("./+/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
@@ -59,12 +59,14 @@ for i in range(peak_use_data.shape[0]-1):
         save_number = save_number + 1
 
 
+
+#From here is normalizatino to 0-255
 save_number = 0
 
 for i in range(peak_use_data.shape[0]-1):
     #print(peak_use_data.iloc[i][0])
     for_save = ecg_use_data.ix[peak_use_data.iloc[i][0]:peak_use_data.iloc[i+1][0]]
-    for_save = signal.resample(for_save, 900)
+    for_save = signal.resample(for_save, 784)
     scaler = MinMaxScaler(feature_range=(0, 255))
     scaler.fit(for_save)
     for_save = scaler.transform(for_save)
@@ -96,4 +98,49 @@ for i in range(peak_use_data.shape[0]-1):
 
 
 
+#From here is normalizatino to 0-255
+save_number = 0
 
+for i in range(peak_use_data.shape[0]-1):
+    #print(peak_use_data.iloc[i][0])
+    for_save = ecg_use_data.ix[peak_use_data.iloc[i][0]:peak_use_data.iloc[i+1][0]]
+    for_save = signal.resample(for_save, 784)
+    scaler = MinMaxScaler(feature_range=(0, 255))
+    scaler.fit(for_save)
+    for_save = scaler.transform(for_save)
+    for_save = np.reshape(for_save, (28, 28))
+    dmap = sb.heatmap(for_save)
+    for_save=pd.DataFrame(for_save)
+    if peak_type_data.iloc[i][0] == "+":
+        for_save.to_csv("./n+/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
+        plt.savefig("./img+/_"+str(save_number)+"'s data.png")
+        save_number = save_number + 1
+    elif peak_type_data.iloc[i][0] == "F":
+        for_save.to_csv("./nF/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
+        plt.savefig("./imgF/_"+str(save_number)+"'s data.png")
+        save_number = save_number + 1
+    elif peak_type_data.iloc[i][0] == "N":
+        for_save.to_csv("./nN/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
+        plt.savefig("./imgN/_"+str(save_number)+"'s data.png")
+        save_number = save_number + 1
+    elif peak_type_data.iloc[i][0] == "S":
+        for_save.to_csv("./nS/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
+        plt.savefig("./imgS/_"+str(save_number)+"'s data.png")
+        save_number = save_number + 1
+    elif peak_type_data.iloc[i][0] == "V":
+        for_save.to_csv("./nV/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
+        plt.savefig("./imgV/_"+str(save_number)+"'s data.png")
+        save_number = save_number + 1
+    elif peak_type_data.iloc[i][0] == "Q":
+        for_save.to_csv("./nQ/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
+        plt.savefig("./imgQ/_"+str(save_number)+"'s data.png")
+        save_number = save_number + 1
+    elif peak_type_data.iloc[i][0] == "~":
+        for_save.to_csv("./n~/_"+str(save_number)+"'s data.csv",mode = 'w', header=False, index=False)
+        plt.savefig("./img~/_"+str(save_number)+"'s data.png")
+        save_number = save_number + 1
+    else :
+        print("Undecided : "+ str(i))
+        save_number = save_number + 1
+        
+    plt.show()
